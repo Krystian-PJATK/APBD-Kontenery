ac0610d834a7e8eaf36a7bfc29246855c9c15250
Krystian-PJATK <s30354@pjwstk.edu.pl>
2025-03-24 20:32:22 +0100

    Create Kontenerowiec.cs

---
+++ Kontenerowiec.cs
@@ -0,0 +1,9 @@
+namespace APBD_Kontenery;
+
+public class Kontenerowiec
+{
+    Kontener[] kontenery;
+    private float maxSpeed; //knots
+    private uint maxContainers;
+    private uint supportedWeight;
+}
